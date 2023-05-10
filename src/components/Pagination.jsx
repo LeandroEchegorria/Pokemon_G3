c13d50f42487d1ff0218a76684a0c0537aa98ba6
German Troncoso <nnamreg.t@gmail.com>
2023-05-10 12:00:12 -0300

    Implementacion de paginacion

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react' ;
 
-export default function Pagination() {
+export default function Pagination({ handleIdChange, idPokemon }) {
+  const [ pages, setPages ] = useState([])
+
+  useEffect(() => {
+    fetchPagination();
+  }, [idPokemon])
+
+  async function fetchPagination() {
+    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${idPokemon - 2}&limit=3`
+    const res = await fetch(url)
+                  .then(response => response.json())
+                  .catch(err => console.log(err))
+                        
+    setPages([res.results[0], res.results[2]])
+  } 
+
+  const previousPokemon = () =>{
+    handleIdChange(idPokemon - 1)
+  }
+
+  const nextPokemon = () =>{
+    handleIdChange(idPokemon + 1)
+  }
+
   return (
     <div className='flex flex-row py-8 gap-4'>
-      <a href='#' className='inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-lg hover:bg-red-800'>
-        <svg aria-hidden='true' class='w-5 h-5 mr-2' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z' clip-rule='evenodd'></path></svg>
-        Pokemon anterior
+      <a href='#' className='inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-lg hover:bg-red-800' onClick={previousPokemon}>
+        <svg aria-hidden='true' className='w-5 h-5 mr-2' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z' clipRule='evenodd'></path></svg>
+        { pages[0]
+            ? `Nº ${idPokemon - 1} ${pages[0].name}`
+            : ''
+        }
       </a>
-      <a href='#' className='inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-lg hover:bg-red-800'>
-      Pokemon siguiente
-        <svg aria-hidden='true' class='w-5 h-5 ml-2' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z' clip-rule='evenodd'></path></svg>
+      <a href='#' className='inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-lg hover:bg-red-800' onClick={nextPokemon}>
+        { pages[1]
+            ? `Nº ${idPokemon + 1} ${pages[1].name}`
+            : ''  
+        }
+        <svg aria-hidden='true' className='w-5 h-5 ml-2' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z' clipRule='evenodd'></path></svg>
       </a>
     </div>
   )
--- src/components/PokemonPage.jsx
+++ src/components/PokemonPage.jsx
@@ -1,13 +1,20 @@
+import { useState } from 'react'
 import Pokemon from './Pokemon'
 import Pagination from './Pagination'
 import Evolution from './Evolution'
 
 export default function PokemonPage() {
+  const [ idPokemon, setIdPokemon ] = useState(280)
+
+  const handleIdChange = (newId) => {
+    setIdPokemon(newId);
+  }
+
   return (
     <section className='flex flex-col justify-center items-center container mx-auto'>
-      <Pagination></Pagination>
-      <Pokemon idPokemon={280}/>
-      <Evolution></Evolution>
+      <Pagination handleIdChange={handleIdChange} idPokemon={idPokemon}></Pagination>
+      <Pokemon idPokemon={idPokemon}/>
+      <Evolution handleIdChange={handleIdChange}></Evolution>
     </section>
   )
 }
